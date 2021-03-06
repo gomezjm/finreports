8e3f2520474c4b0f0d90c90033fa69684bbade7d
gomezjm <gomez.jm@gmail.com>
2021-03-06 12:35:29 +0100

    agrega funcionalidad para generar graficos

--- degiro.py
+++ degiro.py
@@ -5,12 +5,15 @@ from degiroapi.utils import pretty_json
 from datetime import datetime, timedelta, timezone
 import numpy as np 
 import pandas as pd
+import matplotlib.pyplot as plt
 import krakenex
 import os
 
 
 if not os.path.exists('./csv'):
     os.makedirs('./csv')
+if not os.path.exists('./plots'):
+    os.makedirs('./plots')
 
 # DEGIRO--------
 try:
@@ -111,3 +114,21 @@ except Exception as e:
     filename = './csv/finreport' + today.strftime("%y%m%d") + '.csv'
     de_output.to_csv(filename,index=False)
     print(de_output)
+
+#PLOTTING -------
+
+date_to_filter = today.strftime("%d/%m/%y")
+datefile = today.strftime("%y%m%d")
+a = pd.read_csv(f'./csv/finreport{today.strftime("%y%m%d")}.csv')
+b = a[a['date'] == date_to_filter]
+bb=b[['type','value']]
+cc=pd.pivot_table(bb,values='value',index='type',aggfunc=np.sum,fill_value=0)
+pieplot = cc.plot.pie(y='value',label='',autopct='%1.0f%%',labeldistance=1.2,figsize=(5, 5))
+plt.title(f'Distribucion cartera - {date_to_filter}')
+plt.legend(loc="lower center", bbox_to_anchor=(0.5, -0.15), ncol= 2)
+plt.savefig(f'./plots/dist{datefile}.png')
+barplot = cc.plot.bar(y='value',label='',rot=0,figsize=(5, 5))
+plt.title(f'Valor cartera (€) - {date_to_filter}')
+for index,value in enumerate(list(cc['value'])):
+    plt.annotate(f'{str(int(value))}€',(index,value+50),ha='center',weight='bold')
+plt.savefig(f'./plots/value{datefile}.png')
